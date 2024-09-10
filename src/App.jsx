3699a3194b0434dcb286c967aebeb071f66fa98c
Aya Osama <ayaramadan2011@gmail.com>
2024-09-10 22:56:47 +0300

    add playerboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
+import Player from "./components/Player";
 function App() {
   const [darkMode, setdarkMode] = useState(() => {
     return localStorage.getItem("theme") === "dark";
@@ -26,26 +27,19 @@ function App() {
         <span className="text-blue-500 ">X</span>
         <span className="text-red-500 ">O</span>
         <button onClick={handledarkmode}>
-          <li>
-            {darkMode ? <MdDarkMode /> : <MdOutlineDarkMode />}
-          </li>
+          <li>{darkMode ? <MdDarkMode /> : <MdOutlineDarkMode />}</li>
         </button>
       </header>
       <main className="">
         <div className="m-10 bg-gray-900 w-[50%] ">
-          <div className="m-10 ">
-          <ul className="">
-            <li className="inline-block mr-10 ">
-              <span >Israa</span>
-              <span> X</span>
-            </li>
-            <li className="inline-block">
-              <span> Aya</span>
-              <span>  O</span>
-            </li>
-          </ul>
-          GAME BOARD
-        </div></div>
+          <div className="m-[10%] ">
+            <ul className="">
+            <Player name="Player 1" sym="X" />
+            <Player name="Player 2" sym="O" />
+            </ul>
+            GAME BOARD
+          </div>
+        </div>
         LOG
       </main>
     </>
---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Player({name, sym}) {
+  const [isEdit, setisEdit] = useState(false);
+  const [newName, setNewName] = useState(name);
+  function handleEdit() {
+    setisEdit((isEdit)=>!isEdit);
+  }
+
+  let playname = <span className="mr-10">{newName}</span>;
+
+  if (isEdit) {
+    playname = (
+      <input type="text" required value={newName} onChange={(e)=>setNewName(e.target.value)} className="text-black" />
+    );
+  }
+
+  return (
+    <>
+      <li className="inline-block m-10 ">
+        <span>{playname}</span>
+        <span>{sym} </span>
+        <button onClick={handleEdit}> {isEdit ? "Save" : "Edit"}</button>
+      </li>
+    </>
+  );
+}
+
+export default Player;
